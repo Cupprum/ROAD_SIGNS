0f67134db91b745b69b437374fbe0552bf593ed1
Cupprum <branisa.samuel@windowslive.com>
2018-08-14 14:26:32 +0200

    redirecting,flask( request.form[btn] ) is made here much easier

--- project/polls/views.py
+++ project/polls/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 from django.template import loader
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import redirect
 
 
+@require_http_methods(["GET", "POST"])
 def index(request):
     print(request.method)
     if request.method == 'GET':
@@ -9,6 +12,18 @@ def index(request):
         context = {'name': 'milujem simonku', 'orientation': 'simonkofil'}
         return HttpResponse(template.render(context, request))
 
+    elif request.method == 'POST':
+        print(request.POST)
+        if 'test1' in request.POST:
+            return HttpResponse('THIS IS HOW IT WORKS')
 
+        elif 'redirect' in request.POST:
+            return redirect('index2')
+
+        else:
+            return HttpResponse('SOMETHING WENT WRONG')
+
+
+@require_http_methods(["GET", "POST"])
 def index2(request):
     return HttpResponse('skuska 123')
